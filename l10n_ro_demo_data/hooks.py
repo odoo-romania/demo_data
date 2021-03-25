51f238fc8ea7cfbdbe3c4f515fc06e7922885cf2
Mihai Fekete <feketemihai@odooerpromania.ro>
2021-03-25 22:46:44 +0200

    Update demo data

--- l10n_ro_demo_data/hooks.py
+++ l10n_ro_demo_data/hooks.py
@@ -1,16 +1,19 @@
 # Copyright 2020 NextERP Romania SRL
 # License OPL-1.0 or later
 
-from odoo import api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
 
 def pre_init_hook(cr):
-    cr.execute("""
+    cr.execute(
+        """
     UPDATE res_company
     SET anglo_saxon_accounting=True,
         romanian_accounting=True,
         stock_acc_price_diff=True
-    """)
+    """
+    )
+
 
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
